b3cadfe29a00b8017230d86c1b622f8cfa80fc5c
Diego Honorio <diegohonoriiio@gmail.com>
2024-03-14 17:44:46 -0300

    adicionado a logica para impressão dos dados dos eventos, falta resolver o bug onde os dados não estao sendo impressos na orem certa

--- modulos/database/print_data_base_eventos.py
+++ modulos/database/print_data_base_eventos.py
@@ -9,7 +9,7 @@ class PrintBancoDeDadosEventos:
             conn = sqlite3.connect(nome_banco)
             consulta = conn.cursor()
 
-            # Execulta uma consulta para selecionar todos os dados da tabela 'pessoas'
+            # Execulta uma consulta para selecionar todos os dados da tabela 'eventos'
             consulta.execute("SELECT * FROM eventos")
 
             # Recuperar os dados
@@ -17,7 +17,8 @@ class PrintBancoDeDadosEventos:
 
             # Imprimir dados
             for linha in dados:
-                print(linha)
+                print("ID do Evento: {}, Título: {}, Descrição: {}, Data: {}, Hora do Evento: {}, Local do Evento: {}, Categoria: {}, Organizador: {}, Contato: {}, Inscrição: {}, Custo: {}, Rua: {}, Bairro: {}, Cidade: {}, Estado: {}, CEP: {}, E-mail: {}".format(
+                    linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], linha[9], linha[10], linha[11], linha[12], linha[13], linha[14], linha[15], linha[16]))
 
             # Fechar a conexão com o banco de dados 
             conn.close()
@@ -27,3 +28,9 @@ class PrintBancoDeDadosEventos:
 # Criando uma instância da classe e chamando o método
 pb = PrintBancoDeDadosEventos()
 pb.printar_dados_eventos('cadastro_eventos.db')
+
+
+
+
+
+
