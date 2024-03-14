5d047add53618846e828d97ff649fae7d8c56d19
Diego Honorio <diegohonoriiio@gmail.com>
2024-03-14 16:19:58 -0300

    corrigido o problema que, se o usuario selecionasse a opção de printar eventos ou usuarios e não tivesse um banco de dados criado ou usuarios e eventos cadastrados gerava um erro e criava um banco de dados novo. problema corrigido

--- modulos/database/print_data_base_eventos.py
+++ modulos/database/print_data_base_eventos.py
@@ -1,25 +1,29 @@
+import os
 import sqlite3
 
-
-class PrintBancoDeDadosEventos():
+class PrintBancoDeDadosEventos:
     @staticmethod
-    def printar_dados_eventos():
-        # Conectar ao banco de dados
-        conn = sqlite3.connect('cadastro_eventos.db')
-        consulta = conn.cursor()
+    def printar_dados_eventos(nome_banco):
+        if os.path.exists(nome_banco):
+            # Conectar ao banco de dados
+            conn = sqlite3.connect(nome_banco)
+            consulta = conn.cursor()
 
-        # Execulta uma consulta para selecionar todos os dados da tabela 'pessoas'
-        consulta.execute("SELECT * FROM eventos")
+            # Execulta uma consulta para selecionar todos os dados da tabela 'pessoas'
+            consulta.execute("SELECT * FROM eventos")
 
-        # Recuperar os dados
-        dados = consulta.fetchall()
+            # Recuperar os dados
+            dados = consulta.fetchall()
 
-        # Imprimir dados
-        for linha in dados:
-            print(linha)
+            # Imprimir dados
+            for linha in dados:
+                print(linha)
 
-        # Fechar a conexão com o banco de dados 
-        conn.close()
+            # Fechar a conexão com o banco de dados 
+            conn.close()
+        else:
+            print('Nenhum cadastro de eventos encontrado.')
 
+# Criando uma instância da classe e chamando o método
 pb = PrintBancoDeDadosEventos()
-pb.printar_dados_eventos()
+pb.printar_dados_eventos('cadastro_eventos.db')
--- modulos/database/print_data_base_usuarios.py
+++ modulos/database/print_data_base_usuarios.py
@@ -1,25 +1,29 @@
+import os
 import sqlite3
 
-
-class PrintBancoDeDadosUsuarios():
+class PrintBancoDeDadosUsuarios:
     @staticmethod
-    def printar_dados_usuarios():
-        # Conectar ao banco de dados
-        conn = sqlite3.connect('cadastro.db')
-        consulta = conn.cursor()
+    def printar_dados_usuarios(nome_banco):
+        if os.path.exists(nome_banco):
+            # Conectar ao banco de dados
+            conn = sqlite3.connect(nome_banco)
+            consulta = conn.cursor()
 
-        # Execulta uma consulta para selecionar todos os dados da tabela 'pessoas'
-        consulta.execute("SELECT * FROM pessoas")
+            # Execulta uma consulta para selecionar todos os dados da tabela 'pessoas'
+            consulta.execute("SELECT * FROM pessoas")
 
-        # Recuperar os dados
-        dados = consulta.fetchall()
+            # Recuperar os dados
+            dados = consulta.fetchall()
 
-        # Imprimir dados
-        for linha in dados:
-            print(linha)
+            # Imprimir dados
+            for linha in dados:
+                print(linha)
 
-        # Fechar a conexão com o banco de dados 
-        conn.close()
+            # Fechar a conexão com o banco de dados 
+            conn.close()
+        else:
+            print('Nenhum cadastro de usúario encontrado.')
 
+# Criando uma instância da classe e chamando o método
 pb = PrintBancoDeDadosUsuarios()
-pb.printar_dados_usuarios()
+pb.printar_dados_usuarios('cadastro.db')
