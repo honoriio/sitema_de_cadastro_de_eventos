ca541178df74b780283c28fd5a082b6e35d28bc5
Diego Honorio <diegohonoriiio@gmail.com>
2024-03-17 14:18:29 -0300

    foi marrumado o bug que retornava dados incorretos dos eventos

--- modulos/database/print_data_base_eventos.py
+++ modulos/database/print_data_base_eventos.py
@@ -18,7 +18,7 @@ class PrintBancoDeDadosEventos:
             # Imprimir dados
             for linha in dados:
                 print("ID do Evento: {}, Título: {}, Descrição: {}, Data: {}, Hora do Evento: {}, Local do Evento: {}, Categoria: {}, Organizador: {}, Contato: {}, Inscrição: {}, Custo: {}, Rua: {}, Bairro: {}, Cidade: {}, Estado: {}, CEP: {}, E-mail: {}".format(
-                    linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], linha[9], linha[10], linha[11], linha[12], linha[13], linha[14], linha[15], linha[16]))
+                    linha[17], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], linha[9], linha[10], linha[11], linha[12], linha[13], linha[14], linha[15], linha[16]))
 
             # Fechar a conexão com o banco de dados 
             conn.close()
